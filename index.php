7f43fe0462dd7c941c35a85220b3984dadf3efd4
cristiano-acosta <cristiano-acosta@hotmail.com>
2015-12-04 15:06:21 -0200

    Adicionando as Queryes

--- index.php
+++ index.php
@@ -133,35 +133,35 @@
           <div class="container">
             <div class="row">
               <!-- TAB NAVIGATION -->
-              <ul class="col-md-3 nav nav-tabs" role="tablist">
-                <li class="active">
-                  <a href="#tab1" role="tab" data-toggle="tab">Tab1</a>
-                  
-                </li>
-                <li>
-                  <a href="#tab2" role="tab" data-toggle="tab">Tab2</a>
-                </li>
-                <li>
-                  <a href="#tab3" role="tab" data-toggle="tab">Tab3</a>
-                </li>
+              <ul class="col-md-3 nav-tabs" role="tablist">
+                <?php
+                  $index = 0;
+                  $post_type = 'servicos';
+                $Query = new WP_Query(array('post_type' => $post_type, 'post_status' => 'publish', 'posts_per_page' => -1));
+                if ($Query->have_posts()) { while ($Query->have_posts()) : $Query->the_post();
+                  $index++
+                ?>
+                  <li class="<?php if ($index == 0 ) echo 'active' ?>">
+                    <a href="#<?php the_slug() ?>" role="tab" data-toggle="tab"><?php the_title() ?></a>
+                    <?php the_excerpt_limited(24) ?>
+                  </li>
+                <?php endwhile; }  wp_reset_query(); ?>
+
+
               </ul>
               <!-- TAB CONTENT -->
               <div class="col-md-9 tab-content">
-                <div class="active tab-pane fade in" id="tab1">
-                  <h2>Tab1</h2>
-
-                  <p>Lorem ipsum.</p>
-                </div>
-                <div class="tab-pane fade" id="tab2">
-                  <h2>Tab2</h2>
-
-                  <p>Lorem ipsum.</p>
-                </div>
-                <div class="tab-pane fade" id="tab3">
-                  <h2>Tab3</h2>
-
-                  <p>Lorem ipsum.</p>
-                </div>
+                <?php
+                  $index = 0;
+                  $post_type = 'servicos';
+                  $Query = new WP_Query(array('post_type' => $post_type, 'post_status' => 'publish', 'posts_per_page' => -1));
+                  if ($Query->have_posts()) { while ($Query->have_posts()) : $Query->the_post();
+                    $index++
+                    ?>
+                    <div class="<?php if ($index == 0 ) echo 'active' ?> tab-pane fade in" id="<?php the_slug() ?>">
+                      <h2><?php the_title() ?></h2>
+                    </div>
+                  <?php endwhile; }  wp_reset_query(); ?>
               </div>
             </div>
           </div>
